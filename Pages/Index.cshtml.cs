5251ad8f18b14e36beeac555ce51b2aa025dbfba
GUPTAGU <gunjan.gupta@thoughtworks.com>
2023-10-19 22:42:59 +0530

    fetch secret from vault and show as string.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -10,7 +10,7 @@ public class IndexModel : PageModel
 {
 
     public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
-    public string? vaultSecret => CreateSecretInKeyVault();
+    public string vaultSecret => CreateSecretInKeyVault();
 
     private readonly ILogger<IndexModel> _logger;
 
@@ -19,11 +19,11 @@ public class IndexModel : PageModel
         _logger = logger;
     }
     
-    public string? CreateSecretInKeyVault()
+    public string CreateSecretInKeyVault()
     {
         var client = new SecretClient(new Uri("https://exampleTestKeyVault.vault.azure.net/"), new DefaultAzureCredential());
-        var secret = client.GetSecret("test-secret");
-        return secret.Value.ToString();
+        KeyVaultSecret secret = client.GetSecret("test-secret");
+        return secret.Value;
     }
 
     public void OnGet()
